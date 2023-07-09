75f995dfdc19d3c28a4b4b8f71a0c47e9b63fc2e
k1nho <kinhong99@gmail.com>
2023-07-09 13:28:27 -0400

    tea cmd

--- main.go
+++ main.go
@@ -2,31 +2,62 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 const url = "localhost:4000"
 
+type errMsg struct{ err error }
+type httpResponseMsg string
+type statusMessage int
+
+func (e errMsg) Error() string {
+	return e.err.Error()
+}
+
+func checkserver() tea.Msg {
+	c := http.Client{Timeout: 10 * time.Second}
+	res, err := c.Get(url)
+	if err != nil {
+		return errMsg{err: err}
+	}
+
+	return statusMessage(res.StatusCode)
+
+}
+
 type Model struct {
 	requestURL string
-	response   interface{}
+	response   string
+	status     statusMessage
+	error      error
 }
 
 func initialModel() Model {
 	return Model{
 		requestURL: "localhost:4000/v1/api",
-		response:   "hello from endpoint",
+		response:   "",
 	}
 }
 
 func (m Model) Init() tea.Cmd {
-	return nil
+	return checkserver
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+
+	case statusMessage:
+		m.status = msg
+		return m, tea.Quit
+
+	case errMsg:
+		m.error = msg
+		return m, tea.Quit
 
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -39,9 +70,17 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	s := "BlazeMorph"
+	if m.error != nil {
+		return fmt.Sprintf("Could not fetch: %v\n", m.error)
+	}
 
-	return s
+	s := fmt.Sprintf("Checking url... %s", url)
+
+	if m.status > 0 {
+		s += fmt.Sprintf("%d %s!", m.status, http.StatusText(int(m.status)))
+	}
+
+	return "\n" + s + "\n\n"
 }
 
 func main() {
